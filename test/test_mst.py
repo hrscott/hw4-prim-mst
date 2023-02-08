422a72af89c80d1b4a51d19340dbc84ae9ed201b
Henry Scott <106702296+hrscott@users.noreply.github.com>
2023-02-07 21:51:22 -0800

    Update test_mst.py

--- test/test_mst.py
+++ test/test_mst.py
@@ -1,3 +1,4 @@
+#modules used
 import sys
 sys.path.append('../')
 
@@ -6,56 +7,6 @@ import numpy as np
 from mst import Graph
 from sklearn.metrics import pairwise_distances
 import networkx as nx
-
-def test_minimum_spanning_tree():
-    adjacency_matrix = np.array([
-        [0, 2, 0, 6, 0],
-        [2, 0, 3, 0, 5],
-        [0, 3, 0, 0, 0],
-        [6, 0, 0, 0, 0],
-        [0, 5, 0, 0, 0]
-    ])
-    graph = Graph(adjacency_matrix)
-    graph.construct_mst()
-    mst = graph.mst
-    
-    # Verify that it spans all the vertices of the graph
-    assert np.count_nonzero(mst) == len(mst) - 1
-
-    # Check the total weight of the tree
-    #total_weight = 0
-    #for i in range(len(mst)):
-        #for j in range(i + 1, len(mst)):
-            #if mst[i][j] != 0:
-                #total_weight += mst[i][j]
-    #assert total_weight == 14
-
-    # Check that it's a tree
-    #num_edges = 0
-    #for i in range(len(mst)):
-        #for j in range(i + 1, len(mst)):
-            #if mst[i][j] != 0:
-                #num_edges += 1
-    #assert num_edges == len(mst) - 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ###################
 def check_mst(adj_mat: np.ndarray, 
@@ -72,28 +23,39 @@ def check_mst(adj_mat: np.ndarray,
             total += mst[i, j]
     assert approx_equal(total, expected_weight), 'Proposed MST has incorrect expected weight'
 
-    
-    # Ensure that all elements on the main diagonal are zero
-    #for i in range(mst.shape[0]):
-        #assert mst[i, i] == 0, 'Diagonal elements of MST matrix should be zero'
+    ### additional assertions ###
 
-    # Ensure that all edges in the MST are non-negative
-    #for i in range(mst.shape[0]):
-        #for j in range(mst.shape[1]):
-            #assert mst[i, j] >= 0, 'MST contains negative edge weight'
+    #1: Ensure that all elements on the main diagonal are zero
+    for i in range(mst.shape[0]):
+        assert mst[i, i] == 0, 'Diagonal elements of MST matrix should be zero'
+
+    #2: Ensure that all edges in the MST are non-negative
+    for i in range(mst.shape[0]):
+        for j in range(mst.shape[1]):
+            assert mst[i, j] >= 0, 'MST contains negative edge weight'
     
-    # Ensure that all edges in the original graph are either in the MST or are zero
-    #for i in range(adj_mat.shape[0]):
-        #for j in range(adj_mat.shape[1]):
-            #assert adj_mat[i, j] == 0 or mst[i, j] > 0, 'Original graph contains edge not present in MST'
     
-    # Check the expected weight of the MST
-    #expected_weight = 0
-    #for i in range(mst.shape[0]):
-        #for j in range(i+1):
-            #expected_weight += mst[i, j]
-    #expected_weight = expected_weight / 2
-    #assert abs(expected_weight - expected_weight) < 0.0001, 'Proposed MST has incorrect expected weight'
+    #3: Check that the MST is connected by ensuring that there is a path between any two vertices in the graph
+    def is_connected(adj_mat):
+        n = adj_mat.shape[0]
+        visited = [False] * n
+        queue = [0]
+        visited[0] = True
+        while queue:
+            u = queue.pop(0)
+            for v in range(n):
+                if adj_mat[u][v] and not visited[v]:
+                    visited[v] = True
+                    queue.append(v)
+        return all(visited)
+
+    assert is_connected(mst), 'MST is not connected'
+    
+    #4: Check that the number of edges in the MST is equal to the number of vertices minus 1, i.e.
+    n = adj_mat.shape[0]
+    num_edges = np.count_nonzero(mst) // 2
+    assert num_edges == n - 1, 'Incorrect number of edges in MST'
+
 
 
 def test_mst_small():
@@ -113,9 +75,7 @@ def test_mst_single_cell_data():
     
     Unit test for the construction of a minimum spanning tree using single cell
     data, taken from the Slingshot R package.
-
     https://bioconductor.org/packages/release/bioc/html/slingshot.html
-
     """
     file_path = './data/slingshot_example.txt'
     coords = np.loadtxt(file_path) # load coordinates of single cells in low-dimensional subspace
@@ -126,7 +86,8 @@ def test_mst_single_cell_data():
 
 
 def test_construct_mst():
-    # Test 1: Construct MST from a connected adjacency matrix
+    # Test 1: can my function construct an MST from a connected adjacency matrix
+    # testing via direct comparison with nx MST output
     adjacency_mat = np.array([[0, 2, 0, 6, 0],
                               [2, 0, 3, 8, 5],
                               [0, 3, 0, 0, 7],
@@ -138,7 +99,8 @@ def test_construct_mst():
     expected_mst = nx.to_numpy_array(expected_mst)
     np.testing.assert_array_equal(graph.mst, expected_mst)
     
-    # Test 2: Construct MST from an adjacency matrix with disconnected nodes
+    # Test 2: can my function construct an MAST from an adjacency matrix with disconnected nodes
+    # testing via direct comparison with nx MST output
     adjacency_mat = np.array([[0, 2, 0, 6, 0],
                               [2, 0, 3, 8, 5],
                               [0, 3, 0, 0, 7],
@@ -149,5 +111,4 @@ def test_construct_mst():
     expected_mst = nx.minimum_spanning_tree(nx.from_numpy_array(adjacency_mat))
     expected_mst = nx.to_numpy_array(expected_mst)
     np.testing.assert_array_equal(graph.mst, expected_mst)
-    
     
